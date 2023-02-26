44d6b020bb0342533fba9357aee8730e089db406
Jack Rattanasith <jrattanasith@gmail.com>
2023-02-26 01:50:14 -0800

    added some more functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+// Slideshow Physics
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('.navbar');
 
 hamburgerMenu.addEventListener('click', () => {
   navbar.classList.toggle('show');
 });
+
+// Slideshow Timer
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slideshow-container img');
+const interval = setInterval(autoSlide, 3000);
+
+function autoSlide() {
+  slides[slideIndex].classList.remove('active');
+  slideIndex = (slideIndex + 1) % slides.length;
+  slides[slideIndex].classList.add('active');
+}
+
+const slideshowContainer = document.querySelector('.slideshow-container');
+slideshowContainer.addEventListener('mouseenter', () => clearInterval(interval));
+slideshowContainer.addEventListener('mouseleave', () => interval = setInterval(autoSlide, 3000));
